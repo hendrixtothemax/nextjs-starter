ba69d5f38d4799d753abfdd406bb679c4ff2a60e
Alex Hendrix <hendrixtothemax38@gmail.com>
2026-02-06 22:21:19 -0700

    added supabase basic helper functions & proxy

---
+++ proxy.ts
@@ -0,0 +1,37 @@
+import { createServerClient } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
+
+export async function proxy(request: NextRequest) {
+  const response = NextResponse.next()
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll().map((c) => ({ name: c.name, value: c.value }))
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            response.cookies.set(name, value, options)
+          })
+        },
+      },
+    }
+  )
+
+  // Refresh session if expired - 30 seconds before expiration
+  await supabase.auth.getUser()
+
+  return response
+}
+
+export const config = {
+  matcher: [
+    '/',
+    '/about',
+    '/blog',
+    '/contact',
+  ],
+  ignoredRoutes: ['/_next/static', '/_external', '/api/auth/callback'],
+}
